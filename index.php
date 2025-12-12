9e320838dcaadb95a31d40741a789f71b7398126
Markus <ipa23.m.spulis@vtdt.edu.lv>
2025-12-12 08:50:27 +0200

    Klases!

--- index.php
+++ index.php
@@ -10,3 +10,45 @@
 
 </body>
 </html>
+
+<?php
+class Video {
+    public $id;
+    public $url;
+    public $autora_id;
+    public $nosaukums;
+
+    public function __construct($id, $url, $autora_id, $nosaukums) {
+        $this->id = $id;
+        $this->url = $url;
+        $this->autora_id = $autora_id;
+        $this->nosaukums = $nosaukums;
+    }
+}
+class Lietotajs {
+    public $id;
+    public $lietotajvards;
+    public $epasts;
+    public $parole;
+
+    public function __construct($id, $lietotajvards, $epasts, $parole) {
+        $this->id = $id;
+        $this->lietotajvards = $lietotajvards;
+        $this->epasts = $epasts;
+        $this->parole = $parole;
+    }
+}
+class Komentars {
+    public $id;
+    public $video_id;
+    public $autora_id;
+    public $teksts;
+
+    public function __construct($id, $video_id, $autora_id, $teksts) {
+        $this->id = $id;
+        $this->video_id = $video_id;
+        $this->autora_id = $autora_id;
+        $this->teksts = $teksts;
+    }
+}
+?>
